4c7f53c3d13bee09817a12a005a18ab8d075aa77
Kami-san <1.dankon.1@gmail.com>
2021-08-02 22:47:42 +0300

    connect ot new mongo database

--- config/index.js
+++ config/index.js
@@ -2,8 +2,6 @@ const { DBConfig } = require('./.secret_db.js');
 const config = {};
 
 config.PORT = process.env.PORT || 5000;
-
-/* New config */
 config.DB = DBConfig;
 
 config.CONNECTION_STRING = `${config.DB.sertificate}://${config.DB.userName}:${config.DB.password}@${config.DB.dbName}.w2d7e.mongodb.net/${config.DB.dbName}?retryWrites=true&w=majority`;
