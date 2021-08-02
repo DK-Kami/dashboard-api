d910b87904cee3366f1ba207a44e1e94fc748e69
Kami-san <1.dankon.1@gmail.com>
2021-08-02 22:07:40 +0300

    change db config for connect with db on server

--- config/index.js
+++ config/index.js
@@ -1,14 +1,11 @@
+const { DBConfig } = require('./.secret_db.js');
 const config = {};
 
 config.PORT = process.env.PORT || 5000;
 
-config.DB = {
-  sertificate: 'mongodb',
-  baseUrl: 'localhost',
-  dbName: 'dashboard',
-  port: '27017',
-};
+/* New config */
+config.DB = DBConfig;
 
-config.CONNECTION_STRING = `${config.DB.sertificate}://${config.DB.baseUrl}:${config.DB.port}/${config.DB.dbName}`;
+config.CONNECTION_STRING = `${config.DB.sertificate}://${config.DB.userName}:${config.DB.password}@${config.DB.dbName}.w2d7e.mongodb.net/${config.DB.dbName}?retryWrites=true&w=majority`;
 
 module.exports = config;
