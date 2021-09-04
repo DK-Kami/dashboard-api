b9431a943b1510495753f8fe68759b7d80252237
Kami-san <1.dankon.1@gmail.com>
2021-09-04 20:28:29 +0300

    return mock data

--- app/User/User.js
+++ app/User/User.js
@@ -68,7 +68,8 @@ UserModel.methods.getProfile = async function(callback) {
 
   await Promise.all(info, accounts);
 
-  const avatar = gravatar.url(this.email, { s: 200 });
+  // const avatar = gravatar.url(this.email, { s: 200 });
+  const avatar = gravatar.url('1.dankon.1@gmail.com', { s: 200 });
 
   return callback(null, {
     _id: this._id,
@@ -76,8 +77,30 @@ UserModel.methods.getProfile = async function(callback) {
     email: this.email,
     url: this.url,
     avatar,
-    accounts: accounts.map(getCurrentArray(accountUsers, 'account')),
-    info: info.map(getCurrentArray(infoUsers, 'type')),
+    // accounts: accounts.map(getCurrentArray(accountUsers, 'account')),
+    accounts: [
+      
+    ],
+    info: {
+      specialisations: [
+        { _id: 1, name: 'Frontend' },
+        { _id: 2, name: 'Backend' },
+      ],
+      technologies: [
+        { _id: 1, name: 'JS' },
+        { _id: 2, name: 'HTML' },
+        { _id: 2, name: 'CSS' },
+        { _id: 2, name: 'C#' },
+      ],
+      educations: [
+        { _id: 1, name: 'Ebaniy rot', years: '2008 - 2012', position: 'Web-developer', photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmemepedia.ru%2Febanyj-rot-etogo-kazino%2F&psig=AOvVaw2XtpULLVv5JfBEIJ9Qg5Vc&ust=1630858778629000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDvpMTc5fICFQAAAAAdAAAAABAD' },
+      ],
+      work: [
+        { _id: 1, name: 'Tetris Co', years: '2012 - 2019', position: 'Web-developer', photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FTetriscreativethemes%2F&psig=AOvVaw2LXF1Qc1CEZsHIs_ZrSFTB&ust=1630858891653000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJC3xfzc5fICFQAAAAAdAAAAABAD' },
+        { _id: 2, name: 'Pocky', years: '2019 - now', position: 'Backend developer', photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sima-land.ru%2F4387883%2Fpalochki-pocky-glico-v-shokolade-47-g%2F&psig=AOvVaw2gm-obF8ihENTVz1NpUhWU&ust=1630858874458000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLi--PLc5fICFQAAAAAdAAAAABAD' },
+      ]
+    },
+    // info: info.map(getCurrentArray(infoUsers, 'type')),
   });
 };
 
